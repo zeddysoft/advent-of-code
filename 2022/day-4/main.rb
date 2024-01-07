a911f26c6f62d208e62cb3ba15566ee87544c6ea
zeddysoft <azeez.taiwo.c@premise.com>
2024-01-07 14:40:38 +0000

    Add day 1 part 1 2023 solution

--- 2022/day-4/main.rb
+++ 2022/day-4/main.rb
@@ -6,7 +6,7 @@ def part_one
         first_pair, second_pair = line.split(",")
         a1, b1 = first_pair.split("-").map(&:to_i)
 
-        a2, b2 = second_pair.split("-").map(&:to_i)
+        a2, b2 = second_pair.spl]it("-").map(&:to_i)
 
         if (a2 >= a1 && b2 <= b1) || (a1 >= a2 && b1 <= b2)
             sum += 1
